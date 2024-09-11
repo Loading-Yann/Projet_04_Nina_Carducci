52ac1f18e5304b04b9323b966b14babbe4988455
Loading-Yann <yann.jadeau@gmail.com>
2024-09-11 09:50:13 +0200

    purge du css et remaniement du head avec preload

---
+++ purgecss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './index.html',
+    './assets/maugallery.js',
+    './assets/script.js',
+  ],
+  css: ['./assets/style.css'],
+  output: './assets/purged.css',
+}
