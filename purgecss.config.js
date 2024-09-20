f714ffaee5858c77773acf38d5abd6f30149c0f7
Loading-Yann <yann.jadeau@gmail.com>
2024-09-20 09:09:48 +0200

    supression de la config purge.config.css

--- purgecss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    './index.html',
-    './assets/maugallery.js',
-    './assets/script.js',
-  ],
-  css: ['./assets/style.css'],
-  output: './assets/purged.css',
-}
